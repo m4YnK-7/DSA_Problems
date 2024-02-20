1c0137a5359f8e7f5f48eb91f9aa07aad04cd1da
Mayank Singh <mayanksingh4370@gmail.com>
2024-02-20 12:03:54 +0530

    Corrected GD_Fractional_Knapsack

--- Sem 4 DAA/GD_Fractional_Knapsack.cpp
+++ Sem 4 DAA/GD_Fractional_Knapsack.cpp
@@ -1,49 +1,74 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void sorting(int n,float* values,int* profit,int* weight,int *x);
+
 int main(){
+    // For easier I/O.
+    // #ifndef IO
+    // freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+    // #endif
+
+    // Initialising data
     int n;
-    cout<<"Enter number of items ";
+    cout<<"Enter number of items\n";
     cin>>n;
-    int M;
-    cout<<"Enter the M of the knapsack ";
-    cin>>M;
+    int total_capacity,current_capacity=0;
+    cout<<"Enter the M of the knapsack \n";
+    cin>>total_capacity;
 
-    int p[n],weight[n];
-    for(int i=0;i<n;i++){
-        cout<<"Enter the profits of the item "<<i+1<<": ";
-        cin>>p[i];
-    }
+    int profit[n],weight[n],x[n] = {0};
+    float values[n] = {0}, total_profit = 0;
 
     for(int i=0;i<n;i++){
-        cout<<"Enter the weight of the item "<<i+1<<": ";
+        cout<<"Enter the profits of the item "<<i+1<<": \n";
+        cin>>profit[i];
+        cout<<"Enter the weight of the item "<<i+1<<": \n";
         cin>>weight[i];
+        values[i] = float(weight[i])/(float)profit[i];
     }
 
-    cout<<"Maximum profit is ";
-}
+    // Sorting all arrays based on values
+    sorting(n,values,profit,weight,x);
 
-int sorting(int weight[],int p[],int n){
-    float value[n] = {0};
-    for(int i=0;i<n;i++){
-        value[i] = (float)p[i]/(float)weight[i];
-    }
-}
-
-int* knapsack(int n,int M,int weight[]){
-    int x[n] = {0};
-    int rem_capacity = M;
-
-    for(int i=0;i<n;i++){
-        if(weight[i]<=rem_capacity){
+    // Greedy logic
+    for (int i=0;i<n;i++){
+        if(total_capacity>current_capacity+weight[i]){
+            current_capacity+= weight[i];
+            total_profit += profit[i];
             x[i] = 1;
-            rem_capacity-=weight[i];
         }
-        if(i<=n){
-            x[i] = rem_capacity/weight[i];
-            rem_capacity = 0;
+        else{
+            float frac = (total_capacity-current_capacity)/(float)weight[i] ;
+            x[i] = frac;
+            total_profit += frac*profit[i];
+            break;
         }
     }
 
-    return x;   
+    cout<<"Maximum profit is "<<total_profit;
+    return 0;
 }
+
+void sorting(int n,float* values,int* profit,int* weight,int *x){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(values[j]>values[j+1]){
+                swap(values[j],values[j+1]);
+                swap(profit[j],profit[j+1]);
+                swap(weight[j],weight[j+1]);
+                swap(x[j],x[j+1]);
+            }
+        }
+    }
+    return;
+}
+
+/*
+Example : 
+7
+15
+p = 10,5,15,7,6,18,3
+w = 2,3,5,7,1,4,1
+*/
