baba4033db6a2dbeed826d6780777b4993d80086
Mayank Singh <mayanksingh4370@gmail.com>
2024-02-23 19:13:55 +0530

    Activity_Selection and ASL Done

---
+++ Sem 4 DAA/00_GD_Activity_Selection.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Creating a class for easier implementation 
+class Activity{
+    public:
+    int Activity_no;
+    int start_time;
+    int finish_time;
+
+    Activity(){}
+
+    Activity(int i,int j,int k){
+        Activity_no = i;
+        start_time = j;
+        finish_time = k;
+    }
+};
+
+int main(){
+    // Code for easier I/O.
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    
+    // a,b : temporary variables to input start and finish time
+    int n,a,b;
+    cin>>n;
+    Activity arr[n];
+
+    for(int i=0;i<n;i++){
+        cin>>a>>b;
+        arr[i] = Activity(i+1,a,b);
+    }
+
+    // Sorting by finish time
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            if(arr[j].finish_time>arr[j+1].finish_time){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+
+    // Printing selected activity
+    cout<<"Activity Selected: \n";
+    int current = arr[0].finish_time;
+    cout<<arr[0].Activity_no<<"->";
+    for(int i=0;i<n-1;i++){
+        if(arr[i+1].start_time>current){
+            cout<<arr[i+1].Activity_no<<"->";
+            current = arr[i+1].finish_time;
+        }
+    }
+    cout<<"END";
+
+    return 0;
+}
+
+/*
+Sample Input: 
+6
+0 6
+3 4
+1 2
+5 9
+5 7
+8 9
+
+Output : 
+Activity Selected: 
+3->2->5->6->END
+*/
---
+++ Sem 4 DAA/05_DP_Assembly_Line_Scheduling.cpp
@@ -0,0 +1,83 @@
+#include <iostream> 
+#include <vector> 
+using namespace std; 
+
+/*
+a : Assembly Lines Time
+t : Time for changing line
+cs: current state
+cl: current line
+x1,x2: intial time for both lines
+e1,e2: exit time for both lines
+L : path table
+*/
+
+
+
+int fun(vector<vector<int>> a, vector<vector<int>> t, int cl, int cs, int x1, int x2, int n,int L[][2]){ 
+	// base case 
+	if (cs == n - 1) { 
+		if (cl == 0) { // exiting from (current) line =0 
+			return x1; 
+		} 
+		else // exiting from line 2 
+			return x2; 
+	} 
+	// continue on same line 
+	int same  = fun(a, t, cl, cs + 1, x1, x2, n,L) + a[cl][cs + 1]; 
+  
+	// continue on different line 
+	int diff = fun(a, t, !cl, cs + 1, x1, x2, n,L) + a[!cl][cs + 1] + t[cl][cs + 1]; 
+
+	if(min(same,diff)==same){
+		L[cs][cl] = !cl;
+	}
+	else{
+		L[cs][cl] = cl;
+	}
+
+	return min(same, diff); 
+} 
+
+
+int main() { 
+	int n = 6; // number of station
+
+	vector<vector<int> > a  = { {7,9,3,4,8,4}, {8,5,6,4,5,7} }; 
+	vector<vector<int> > t  = { {0,2,3,1,3,4}, {0,2,1,2,2,1} }; 
+
+	int L[n-1][2];
+			
+	int e1 = 2; 
+	int e2 = 4; 
+	int x1 = 3; 
+	int x2 = 2; 
+	
+	// entry from 1st line 
+	int x = fun(a, t, 0, 0, x1, x2, n,L) + e1 + a[0][0]; 
+	// entry from 2nd line 
+	int y = fun(a, t, 1, 0, x1, x2, n,L) + e2 + a[1][0]; 
+	cout <<"The minimum time required for the assembly is: "<< min(x, y) << endl; 
+
+	// int cl = 1;
+
+	// for(int i=0;i<n-1;i++){
+	// 	for(int j=0;j<2;j++){
+	// 		cout<<L[i][j]+1<<" ";
+	// 	}
+	// 	cout<<endl;
+	// }
+
+	// for(int i=n-2;i>=0;i--){
+	// 	if(L[i][cl]==1){
+	// 		cout<<"2->";
+	// 		cl = !cl;
+	// 	}
+	// 	else{
+	// 		cout<<"1->";
+	// 	}
+	// }
+
+	// path(L,n);
+}
+
